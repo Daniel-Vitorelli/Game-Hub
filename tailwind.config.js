eb41c596b935720ccd769c835d3122512ae637dc
Daniel Vitorelli <danielsenvitorelli@hotmail.com>
2024-10-07 21:38:19 -0300

    adicionei 3 joguinhos

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       backgroundImage: {
         'the-worlds-hardest-game': 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)), url("https://cdn.jogos360.com.br/th/ew/the-worlds-hardest-game-d.jpg?width=200&height=200&aspect_ratio=1:1")',
         'fireboy-watergirl-1': 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)), url("https://cdn.jogos360.com.br/fi/re/fireboy-e-watergirl-in-the-forest-temple-d.jpg?width=200&height=200&aspect_ratio=1:1")',
+        'bad-ice-cream': 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)), url("https://cdn.jogos360.com.br/ba/di/bad-ice-cream-d.jpg?width=200&height=200&aspect_ratio=1:1")',
+        'stumble-guys': 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)), url("https://cdn.jogos360.com.br/st/um/stumble-guys-d.jpg?width=200&height=200&aspect_ratio=1:1")',
+        'geometry-dash-online': 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)), url("https://cdn.jogos360.com.br/ge/om/geometry-dash-online-d.jpg?width=200&height=200&aspect_ratio=1:1")',
       },
     },
   },
